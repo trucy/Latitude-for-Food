3e2978d3578b0c7be9fa5a3f4c780f8c6f2220a6
Trucy <trucy.phan@gmail.com>
2011-02-03 18:15:08 -0800

    added lat/long, operating days and hours for locations

---
+++ locations.js
@@ -0,0 +1,26 @@
+{
+  "location01": {
+    "title":"Fourbarrel",
+    "latitude": 37.767019,
+    "longitude": -122.421961,
+    "dayOpen": [1, 1, 1, 1, 1, 1, 1], // S, M, T, W, R, F, Sa
+    "hourOpen": [8, 7, 7, 7, 7, 7, 7],
+	"hourClose": [8, 8, 8, 8, 8, 8, 8]
+  },
+  "location02": {
+    "title":"Coffee Bar",
+	"latitude": 37.763099,
+	"longitude"-122.411031,
+    "dayOpen": [1, 1, 1, 1, 1, 1, 1], // S, M, T, W, R, F, Sa
+    "hourOpen": [8, 7, 7, 7, 7, 7, 8],
+	"hourClose": [7, 8, 8, 8, 10, 10, 8]
+  },
+  "location03": {
+    "title":"Cheese Board Collective",
+	"latitude":37.879968, 
+	"longitude":-122.26935,
+	"dayOpen": [0, 1, 1, 1, 1, 1, 1], // S, M, T, W, R, F, Sa
+    "hourOpen": [0, 7, 7, 7, 7, 7, 8.5],
+	"hourClose": [0, 1, 6, 6, 6, 6, 5]
+  }
+}
